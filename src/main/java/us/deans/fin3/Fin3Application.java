14e170b74ca599c323c7636531667c72ce70588e
Nigel Deans <ndeans@hotmail.com>
2022-01-07 21:05:38 -0500

    FIN-3 Initial commit from local.

---
+++ src/main/java/us/deans/fin3/Fin3Application.java
@@ -0,0 +1,13 @@
+package us.deans.fin3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fin3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fin3Application.class, args);
+    }
+
+}
---
+++ src/test/java/us/deans/fin3/Fin3ApplicationTests.java
@@ -0,0 +1,13 @@
+package us.deans.fin3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fin3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
